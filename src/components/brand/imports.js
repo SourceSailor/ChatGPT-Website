ce2b56d9762b7cb71b27f20fd943876aef42e96e
Kyle Pickard <123780748+SourceSailor@users.noreply.github.com>
2024-02-29 11:47:46 -0800

    Brand Component
    Created and styled the Brand component

---
+++ src/components/brand/imports.js
@@ -0,0 +1,7 @@
+import google from "../../assets/Google_Logo.png";
+import slack from "../../assets/slack_logo.png";
+import atlassian from "../../assets/atlassian_logo.png";
+import dropbox from "../../assets/dropbox_logo.png";
+import shopify from "../../assets/shopify_logo.png";
+
+export { google, slack, atlassian, dropbox, shopify };
