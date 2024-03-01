2f45276b7e06d8ba5eb5d1d656fc9f9ab9bb3589
Kyle Pickard <123780748+SourceSailor@users.noreply.github.com>
2024-03-01 15:38:05 -0800

    WhatGPT3 Component Creation
    Created and added all of the content to the WhatGPT3 component.
    
    Worked on desktop styles for component.

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./feature.css";
 
-const Feature = () => {
-  return <div>Feature</div>;
+const Feature = ({ title, text, featureClass }) => {
+  return (
+    <div className={`gpt3__features-container__feature `}>
+      <div
+        className={`gpt3__features-container__feature-title ${featureClass}`}
+      >
+        <span className="gpt3__features-container__features-bar"></span>
+        <div className="gpt3__feature-title">
+          <h1>{title}</h1>
+        </div>
+
+        <div className="gpt3__features-container__feature-text">
+          <p>{text}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Feature;
