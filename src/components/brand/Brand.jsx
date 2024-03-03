f77f10d010563e683848f9be6980466d8baaa06b
Kyle Pickard <123780748+SourceSailor@users.noreply.github.com>
2024-03-02 16:40:29 -0800

    WhatGPT3 Component Mobile Styles
    Styling the WhatGPT3 Component for mobile.

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -6,19 +6,19 @@ const Brand = () => {
   return (
     <div className="gpt3__brand container__padding">
       <div>
-        <img src={google} alt="" />
+        <img src={google} alt="Google logo" />
       </div>
       <div>
-        <img src={slack} alt="" />
+        <img src={slack} alt="Slack logo" />
       </div>
       <div>
-        <img src={atlassian} alt="" />
+        <img src={atlassian} alt="Atlassian logo" />
       </div>
       <div>
-        <img src={dropbox} alt="" />
+        <img src={dropbox} alt="Dropbox logo" />
       </div>
       <div>
-        <img src={shopify} alt="" />
+        <img src={shopify} alt="Shopify logo" />
       </div>
     </div>
   );
